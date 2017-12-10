2371e1d56233164b571a02b580feb93f3fd35c53
Jorge Vergara <javebratt@gmail.com>
2017-12-10 15:07:50 -0500

    Adds styles to the authentication pages

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -8,9 +8,10 @@ import {
   NavController
 } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { EmailValidator } from '../../validators/email';
 import { AuthProvider } from '../../providers/auth/auth';
 import { HomePage } from '../home/home';
+import { EmailValidator } from '../../validators/email';
+import firebase from 'firebase/app';
 
 @IonicPage()
 @Component({
@@ -19,7 +20,6 @@ import { HomePage } from '../home/home';
 })
 export class LoginPage {
   public loginForm: FormGroup;
-  public loading: Loading;
   constructor(
     public navCtrl: NavController,
     public loadingCtrl: LoadingController,
@@ -47,33 +47,33 @@ export class LoginPage {
     this.navCtrl.push('ResetPasswordPage');
   }
 
-  loginUser(): void {
+  async loginUser(): Promise<void> {
     if (!this.loginForm.valid) {
       console.log(
         `Form is not valid yet, current value: ${this.loginForm.value}`
       );
     } else {
+      const loading: Loading = this.loadingCtrl.create();
+      loading.present();
+
       const email = this.loginForm.value.email;
       const password = this.loginForm.value.password;
 
-      this.authProvider.loginUser(email, password).then(
-        authData => {
-          this.loading.dismiss().then(() => {
-            this.navCtrl.setRoot(HomePage);
-          });
-        },
-        error => {
-          this.loading.dismiss().then(() => {
-            const alert: Alert = this.alertCtrl.create({
-              message: error.message,
-              buttons: [{ text: 'Ok', role: 'cancel' }]
-            });
-            alert.present();
-          });
-        }
-      );
-      this.loading = this.loadingCtrl.create();
-      this.loading.present();
+      try {
+        const loginUser: firebase.User = await this.authProvider.loginUser(
+          email,
+          password
+        );
+        await loading.dismiss();
+        this.navCtrl.setRoot(HomePage);
+      } catch (error) {
+        await loading.dismiss();
+        const alert: Alert = this.alertCtrl.create({
+          message: error.message,
+          buttons: [{ text: 'Ok', role: 'cancel' }]
+        });
+        alert.present();
+      }
     }
   }
 }
