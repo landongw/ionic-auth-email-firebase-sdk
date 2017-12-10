d2f08a233b0ffec419f12bf5f972b664474a7692
Jorge Vergara <javebratt@gmail.com>
2017-12-10 15:20:46 -0500

    Adds signup page

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,25 +1,71 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SignupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {
+  Alert,
+  AlertController,
+  IonicPage,
+  Loading,
+  LoadingController,
+  NavController
+} from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthProvider } from '../../providers/auth/auth';
+import { HomePage } from '../home/home';
+import { EmailValidator } from '../../validators/email';
+import firebase from 'firebase/app';
 
 @IonicPage()
 @Component({
   selector: 'page-signup',
-  templateUrl: 'signup.html',
+  templateUrl: 'signup.html'
 })
 export class SignupPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public signupForm: FormGroup;
+  constructor(
+    public navCtrl: NavController,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    public authProvider: AuthProvider,
+    formBuilder: FormBuilder
+  ) {
+    this.signupForm = formBuilder.group({
+      email: [
+        '',
+        Validators.compose([Validators.required, EmailValidator.isValid])
+      ],
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(6)])
+      ]
+    });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
-  }
+  async signupUser(): Promise<void> {
+    if (!this.signupForm.valid) {
+      console.log(
+        `Form is not valid yet, current value: ${this.signupForm.value}`
+      );
+    } else {
+      const loading: Loading = this.loadingCtrl.create();
+      loading.present();
 
+      const email = this.signupForm.value.email;
+      const password = this.signupForm.value.password;
+
+      try {
+        const signupUser: firebase.User = await this.authProvider.signupUser(
+          email,
+          password
+        );
+        await loading.dismiss();
+        this.navCtrl.setRoot(HomePage);
+      } catch (error) {
+        await loading.dismiss();
+        const alert: Alert = this.alertCtrl.create({
+          message: error.message,
+          buttons: [{ text: 'Ok', role: 'cancel' }]
+        });
+        alert.present();
+      }
+    }
+  }
 }
