714fa069c890b0622ac3144385b1ba04f8ed6c0f
Jorge Vergara <javebratt@gmail.com>
2017-12-10 14:15:53 -0500

    Creates the authentication provider

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,17 +1,40 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import firebase from 'firebase';
 
-/*
-  Generated class for the AuthProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class AuthProvider {
+  constructor() {}
 
-  constructor(public http: HttpClient) {
-    console.log('Hello AuthProvider Provider');
+  loginUser(email: string, password: string): Promise<firebase.User> {
+    return firebase.auth().signInWithEmailAndPassword(email, password);
   }
 
+  async signupUser(email: string, password: string): Promise<firebase.User> {
+    try {
+      const newUser: firebase.User = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password);
+
+      await firebase
+        .database()
+        .ref(`/userProfile/${newUser.uid}/email`)
+        .set(email);
+      return newUser;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  resetPassword(email: string): Promise<void> {
+    return firebase.auth().sendPasswordResetEmail(email);
+  }
+
+  logoutUser(): Promise<void> {
+    const userId: string = firebase.auth().currentUser.uid;
+    firebase
+      .database()
+      .ref(`/userProfile/${userId}`)
+      .off();
+    return firebase.auth().signOut();
+  }
 }
